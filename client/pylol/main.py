d81acf1b8cdb5eca3397114c7c817d85e4f2ddb8
0xFireball <0xFireball@outlook.com>
2017-02-04 16:23:54 -0800

    simple dummy client

--- client/pylol/main.py
+++ client/pylol/main.py
@@ -4,5 +4,21 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect(('127.0.0.1', 5503))
 
+
+def getpacket(transport):
+    n = False
+    data = ""
+    while not n:
+        r = transport.recv(1).decode()
+        if r != '\n':
+            data += r
+        else:
+            n = True
+    return data
+
+
 # send hello
-sock.sendall('$H|lolpy fake sensor|stream')
+sock.sendall('$H|lolpy fake sensor|stream'.encode())
+ack = getpacket(sock)
+print(ack)
+
