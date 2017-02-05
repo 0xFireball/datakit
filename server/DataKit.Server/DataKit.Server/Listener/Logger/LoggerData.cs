685d1ace7eb3feb2a1eebe60e69250a23f92e248
0xFireball <0xFireball@outlook.com>
2017-02-05 00:51:36 -0800

    use long for timestamp

--- server/DataKit.Server/DataKit.Server/Listener/Logger/LoggerData.cs
+++ server/DataKit.Server/DataKit.Server/Listener/Logger/LoggerData.cs
@@ -12,16 +12,16 @@ namespace DataKit.Server.Listener.Logger
         public string Tag { get; }
 
         [JsonProperty("timestamp")]
-        public DateTimeOffset Timestamp { get; }
+        public long Timestamp { get; }
         public LoggerData(string rawData)        
         {
-            var rawDataParts = rawData.Split(' ');t
+            var rawDataParts = rawData.Split(' ');
             var tag = rawDataParts[1];
             var rawTimestamp = rawDataParts[2];
             var rawDataFloat = rawDataParts[3];
             Tag = tag;
             Data = float.Parse(rawDataFloat);
-            Timestamp = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(rawTimestamp));
+            Timestamp = long.Parse(rawTimestamp);
         }
     }
 }
