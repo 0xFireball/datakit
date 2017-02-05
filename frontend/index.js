065c625852206b76f995ea24a84cecc0d7e3864c
Trevor Salom <trvrsalom@gmail.com>
2017-02-04 21:43:33 -0800

    Switchin away from vue

--- frontend/index.js
+++ frontend/index.js
@@ -1,35 +1,44 @@
 var config = null;
 
 var dk = {
-	initStorage: function() {
+    utils: {
+        initStorage: function() {
+            window.localStorage.dk = window.localStorage.dk === undefined ? {} : window.localStorage.dk;
+        },
+        saveConfig: function() {
+            var inputs = $("input");
+            var configToSave = {};
+            for (var i = 0; i < inputs.length; i++) {
+                var inp = inputs[i];
+                configToSave[inp.name] = inp.value;
+            }
+            window.localStorage.dk = JSON.stringify(configToSave);
+        }
+    },
+    pages: {
+        global: function() {
+          config = JSON.parse(window.localStorage.dk); 
+            dk.utils.initStorage();
+            $("dkheader").replaceWith('<div class="ui top fixed menu" style="position: static !important;"><a class="item" href="index.html"><strong>Data</strong>Kit</a><a class="item" href="devices.html">Devices</a><a class="item" href="config.html">Configure</a></div>');
+        },
+        devices: function() {
 
-	},
-	saveConfig: function() {
-		var inputs = $("input");
-		var configToSave = {};
-		for(var i = 0; i < inputs.length; i++) {
-			var inp = inputs[i];
-			configToSave[inp.name] = inp.value;
-		}
-		Window.localStorage.dk.config = configToSave;
-	}
+        }
+    }
 };
 
-var dkheader = Vue.component('dkheader', {
-	template: '<div class="ui top fixed menu" style="position: static !important;"><a class="item" href="index.html"><strong>Data</strong>Kit</a><a class="item" href="devices.html">Devices</a><a class="item" href="config.html">Configure</a></div>'
-});
-
-var dkdevice = Vue.component('dkdevice', {
-	template: '<tr><td>{{device.id}}</td><td>{{device.name}}<td>Connect</td></tr>'
-});
-
-new Vue({
-	el: "#dkbody"
-});
 
 var configLoaded = function(conf) {
-	config = JSON.parse(conf);
-	console.log(config);
+    config = JSON.parse(conf);
+    console.log(config);
+    var page = window.location.pathname.split("/").pop();
+    page = page.slice(0, page.length - 5);
+    dk.pages.global();
+    try {
+        dk.pages[page]();
+    } catch (err) {
+        console.warn("The '" + page + "' function has not been defined in the dk page list");
+    }
 };
 
 $.get("config.json", configLoaded);
