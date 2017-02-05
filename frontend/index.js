9a8a0cf40b767bd61d9b45ceec56b32dd319db63
Trevor Salom <trvrsalom@gmail.com>
2017-02-05 01:18:50 -0800

    Mid ws frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,17 @@
 var config = null;
 
+var getQueryParams = function(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
 var dk = {
     utils: {
         initStorage: function() {
@@ -13,16 +25,66 @@ var dk = {
                 configToSave[inp.name] = inp.value;
             }
             window.localStorage.dk = JSON.stringify(configToSave);
+        },
+        getURL: function() {
+          return "http://" + config.dk_rpi_ip + ":" + config.dk_rpi_port;
         }
     },
     pages: {
         global: function() {
-          config = JSON.parse(window.localStorage.dk); 
+            config = JSON.parse(window.localStorage.dk);
             dk.utils.initStorage();
             $("dkheader").replaceWith('<div class="ui top fixed menu" style="position: static !important;"><a class="item" href="index.html"><strong>Data</strong>Kit</a><a class="item" href="devices.html">Devices</a><a class="item" href="config.html">Configure</a></div>');
         },
         devices: function() {
+            $.get("http://" + config.dk_rpi_ip + ":" + config.dk_rpi_port + "/r/connected", function(data) {
+                data.forEach(function(device) {
+                    $("#deviceList").append("<tr><td>" + device.name + "</td><td>" + device.units + "</td><td class='ui checkbox'><input type='checkbox' name='" + device.id + "'><label>Include</label></td></tr>");
+                });
+                $(".checkbox").checkbox();
+            });
+        },
+        data: function() {
+          var deviceList = getQueryParams("devices").split(",");
+          $.each(deviceList, function(val) {
+            var did = deviceList[val];
+            $.post(dk.utils.getURL() + "/r/createchannel/" + did, function(channelID) {
+              var newObj = {
+                did: deviceList[val],
+                channel: channelID
+              };
+              newObj.ws = new WebSocket("ws" + dk.utils.getURL().substring(4) + "/ws");
+              newObj.ws.onopen = function() {
+                console.log("Opening: " + newObj.channel);
+                newObj.ws.send(">" + newObj.channel + "\n");
+              };
+              newObj.ws.onmessage = function(evt) {
+                console.log(evt.data);
+              };
+              dk.pageFunctions.data.deviceConnections.push(newObj);
+            });
+          });
+        }
+    },
+    pageFunctions: {
+        devices: {
+            clearDevices: function() {
 
+            },
+            loadDevices: function() {
+                var deviceList = [];
+                $.each($(".checkbox"), function(device) {
+                    device = $($(".checkbox")[device]);
+                    if (device.checkbox('is checked')) {
+                        deviceList.push(device.children().first().attr('name'));
+                    }
+                });
+                var url = deviceList.join(',');
+                window.location.href = "data.html?devices=" + url;
+            }
+        },
+        data: {
+          deviceConnections: []
         }
     }
 };
@@ -37,8 +99,29 @@ var configLoaded = function(conf) {
     try {
         dk.pages[page]();
     } catch (err) {
-        console.warn("The '" + page + "' function has not been defined in the dk page list");
+      console.log(err);
+      console.warn("The '" + page + "' function may not have been defined in the dk page list");
     }
 };
+
+var ws = new WebSocket("ws://localhost:5000/ws");
+
+               ws.onopen = function()
+               {
+                  // Web Socket is connected, send data using send()
+                  console.log("sending");
+                  ws.send(">1f45df1250ca41f8a08eb6e9f71bda90c\n");
+               };
+
+               ws.onmessage = function (evt)
+               {
+                  var received_msg = evt.data;
+                  console.log(received_msg);
+               };
+
+               ws.onclose = function()
+               {
+                  // websocket is closed.
+               };
 
 $.get("config.json", configLoaded);
